af77659ef99b53d5b02abeb92770aa8c53469c61
Dishant Sharma <dishu5570@gmail.com>
2022-06-14 15:56:08 +0530

    server update with nodemail

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { cloudinary } = require('./utils/cloudinary');
+const nodemailer = require('nodemailer')
+require('dotenv').config();
 const connectDB = require('./utils/db.js');
 const User = require('./Models/memberModel.js');
 const express = require('express');
@@ -13,21 +15,78 @@ app.use(cors());
 
 connectDB()
 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_ID,
+        pass: process.env.GMAIL_PASSWORD
+    }
+})
+
+let generatedOtp;
+
 app.get('/api/images', async (req, res) => {
     const members = await User.find({});
     console.log(members)
     res.json(members)
 });
+
+app.post('/api/getotp', async (req, res) => {
+    try {
+        generatedOtp = Math.floor((Math.random() * 1000000) + 54);
+        const { email } = req.body
+        console.log(email, 'hello')
+        let mailOptions = {
+            from: process.env.USER_ID,
+            to: email,
+            subject: 'Let\' get you verified!!',
+            html: `Your OTP For Verification is ${generatedOtp}`,
+        }
+        await transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                return res.json(err)
+            } else {
+                res.status(201).json({ info: 'Otp Send' })
+            }
+        })
+
+    }
+    catch (err) {
+        console.log(err)
+        res.status(404).json(err)
+    }
+})
+
 app.post('/api/upload', async (req, res) => {
     try {
         const fileStr = req.body.data;
-        const { name, email, domain, linkedin, github, twitter, instagram } = req.body
+        const { name, email, domain, linkedin, github, twitter, instagram, otp } = req.body
         const uploadResponse = await cloudinary.uploader.upload(fileStr, {
             upload_preset: 'dev_setups',
         });
+
+        if (+otp !== generatedOtp) {
+            return res.status(400).json({ err: 'OTP Not verified!' })
+        }
         const member = await User.create({ photo: uploadResponse.url, name, email, domain, linkedin, github, twitter, instagram })
-        console.log(member)
-        res.status(201).json(member)
+
+        let mailOptions = {
+            from: process.env.USER_ID,
+            to: email,
+            subject: 'Welcome to the Community!',
+            html: 'Welcome to the Community',
+        }
+
+        await transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                return res.json(err)
+            } else {
+                console.log(member)
+                res.status(201).json(member)
+            }
+        })
+
+
 
     } catch (err) {
         console.error(err);

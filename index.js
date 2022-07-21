45372030de847b127bd7d034d513d944e3e396d6
Dishant Sharma <dishu5570@gmail.com>
2022-07-21 19:52:15 +0530

    cors

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 connectDB()
 
 
